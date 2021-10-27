7f7ddc418c33aaf04c79919210bcc646d207e78e
tallev97 <tallevinzon@gmail.com>
2021-10-27 11:55:27 +0300

    Tal's Commit

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,53 +1,65 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * The class TestThreadCheckArray responsible of testing
+ * 
+ * @author DanielOhayon
+ * @author TalLevinzon
+ * @version 1.0z Build 1 October 26, 2021.
+ * 
+ */
 public class TestThreadCheckArray {
+	/**
+	 * The main method create SharedData object that used like common resource for
+	 * the two threads. The method get input size of array from user, get input
+	 * elements from the user to the array and get input sum "b" element. The method
+	 * check with two threads if there is elements in array that equals together to
+	 * sum "b". If there is solution the thread that find the solution report to the
+	 * second thread with boolean array that indicate the place of the elements that
+	 * equal together to sum b.
+	 *
+	 */
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
-			int num  = input.nextInt();
+			int num = input.nextInt();
 			ArrayList<Integer> array = new ArrayList<Integer>();
 			System.out.println("Enter numbers for array");
 
-			for (int index = 0; index < num; index++) 
+			for (int index = 0; index < num; index++)
 				array.add(input.nextInt());
-			
+
 			System.out.println("Enter number");
 			num = input.nextInt();
-			
+
 			SharedData sd = new SharedData(array, num);
-			
+
 			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
 			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
 			thread1.start();
 			thread2.start();
-			try 
-			{
+			try {
 				thread1.join();
 				thread2.join();
-			} 
-			catch (InterruptedException e)
-			{
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			if (!sd.getFlag())
-			{
+			if (!sd.getFlag()) {
 				System.out.println("Sorry");
 				return;
 			}
-			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
+			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().size());
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+			for (int index = 0; index < sd.getArray().size(); index++)
 				System.out.print(index + "    ");
 			System.out.println();
 			System.out.print("A:    ");
-			for (int index : sd.getArray())
-			{
+			for (int index : sd.getArray()) {
 				System.out.print(index);
 				int counter = 5;
-				while (true)
-				{
+				while (true) {
 					index = index / 10;
 					counter--;
 					if (index == 0)
@@ -59,12 +71,11 @@ public class TestThreadCheckArray {
 
 			System.out.println();
 			System.out.print("C:    ");
-			for (boolean index : sd.getWinArray())
-			{
+			for (boolean index : sd.getWinArray()) {
 				if (index)
 					System.out.print("1    ");
 				else
-					System.out.print("0    ");	
+					System.out.print("0    ");
 			}
 		}
 	}
